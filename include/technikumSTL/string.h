b79d0d024556940242535823d2527a69bd9365c0
chiara.babiak <chiara.babika@accenture.com>
2023-09-27 18:32:11 +0200

    - project done - TODO: Add explanations over comments

--- include/technikumSTL/string.h
+++ include/technikumSTL/string.h
@@ -1,6 +1,112 @@
 #pragma once
+#include <string>
+#include <iostream>
+#include <iterator> // std::forward_iterator_tag
+#include <cstddef>  // std::ptrdiff_t
 
 namespace technikum
 {
-  int add(int, int);
+  class string{
+
+   protected:
+    // this is our char* => composing the string we need
+    // Erstellung eines Strings mit const char*
+    char* class_str;
+    // this is what we need for the length of the string
+    unsigned int str_len = 0;
+
+   public:
+    // we need to iterate through the chars in the "string" we have
+    // this is why I have here a nested Iterator Class
+    class Iterators
+    {
+      using iterator_category = std::forward_iterator_tag; // scan forward multiple times and read/write with value it points to
+      using difference_type = std::ptrdiff_t; // identify distance between it steps
+      using val_type = char*; // type it iterates over
+      using pointer = char*;
+      using reference = char&; // reference to type iterates over
+     private:
+      pointer it;
+     public:
+      Iterators(pointer ptr) : it(ptr) {}
+
+
+      // overload
+      reference operator*() const { return *it; }
+      pointer operator->() { return it; }
+
+      Iterators& operator++() // ++it
+      {
+        it++;
+        return *this;
+      }
+      Iterators operator++(int) // it++
+      {
+        Iterators temp(*this);
+        ++(*this);
+        return temp;
+      }
+      Iterators& operator--() //--it
+      {
+        it--;
+        return *this;
+      }
+      Iterators operator--(int) // it--
+      {
+        Iterators temp(*this);
+        --(*this);
+        return temp;
+      }
+      bool operator==(const Iterators& it2)
+      {
+        return it == it2.it;
+      }
+      bool operator!=(Iterators& it2)
+      {
+        return it != it2.it;
+      }
+      friend bool operator==(const Iterators& it1, const Iterators& it2)
+      {
+        return it1.it == it2.it;
+      }
+      friend bool operator!=(const Iterators& it1, const Iterators it2)
+      {
+        return it1.it != it2.it;
+      }
+    };
+
+
+    // lets create some more stuff for our StringClass
+    // we firstly need constructors and destrcutors
+    string(); // empty constructor
+    // one parameter constructor
+    string(char* str); // simple one parameter char constructor
+    // const char constructor
+    string(const char* str); // the const char constructor
+
+
+    // destructor
+    ~string();
+
+
+    // setter // TODO: what is that ? => eggsplain
+    void setClassStr(const char* str);
+    operator const char* () const; // TODO: what is that even :sob:
+
+
+    // Zusammenfügen von Strings über eine append() member Funktion ==> here is our append function
+    void append(const char* str);
+    // display the string object
+    // Erlauben Sie den Zugriff auf die intern verwaltete
+    // Zeichenkette über die c_str() member Funktion (c string
+    // equivalent)
+    const char* c_str() const;
+    // for the length of our string
+    static size_t getLength(const char* str);
+
+    // how do we iterate through the string ?
+    Iterators begin() const ;
+    Iterators end() const ;
+
+  };
 }
--- src/string.cpp
+++ src/string.cpp
@@ -1,3 +1,81 @@
 #include "technikumSTL/string.h"
 
-int technikum::add(int a, int b) { return a + b; }
+void technikum::string::setClassStr(const char* str)
+{
+  str_len = this->getLength(str);
+  class_str = new char[str_len + 1];
+  for (int i = 0; str[i] != '\0'; i++) class_str[i] = str[i];
+  class_str[str_len] = '\0';
+}
+
+technikum::string::string()
+{
+  class_str = nullptr;
+  str_len = 0;
+}
+
+technikum::string::string(char * str)
+{
+  setClassStr(str);
+  str_len = this->getLength(str);
+}
+
+technikum::string::string(const char* str)
+{
+  setClassStr(str);
+  str_len = this->getLength(str);
+}
+
+// destructor
+technikum::string::~string()
+{
+  if (class_str)
+  {
+    str_len = 0; // set length to 0
+    delete class_str; // delete the object
+  }
+}
+
+// TODO: What happens here even :sob:
+void technikum::string::append(const char* str)
+{
+  int newStrLen = str_len + this->getLength(str);
+  char* newClassStr = new char[newStrLen + 1];
+
+  for (size_t i = 0; i < str_len; i++) newClassStr[i] = class_str[i];
+  int j = 0;
+  for (int i = str_len; i < newStrLen; i++) {
+    newClassStr[i] = str[j];
+    j++;
+  }
+  newClassStr[newStrLen] = '\0';
+  setClassStr(newClassStr);
+  delete[] newClassStr;
+}
+
+const char* technikum::string::c_str() const
+{
+  return class_str;
+}
+
+size_t technikum::string::getLength(const char* str)
+{
+  size_t len = 0;
+  if (str == nullptr) return len; // return 0 if nullptr
+
+  for (int i = 0; str[i] != '\0'; i++) { len++; }
+  return len;
+}
+
+technikum::string::Iterators technikum::string::begin() const
+{
+  return Iterators(&class_str[0]);
+}
+
+technikum::string::Iterators technikum::string::end() const
+{
+  return Iterators(&class_str[str_len]);
+}
+
+
+
